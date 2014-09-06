03538c75e23d01a2cbceb6a745b1fe6e5e5d4292
Dabea <abeis@msn.com>
2014-09-06 00:56:18 -0500

    Update Date-class-ver2.php
    *Strtotime function to use for other function in the class
    *Split function set & get

--- Date-class-ver2.php
+++ Date-class-ver2.php
@@ -6,6 +6,32 @@
         protected $monthString;
         protected $monthAbb;
         protected $date_d;
+        protected $day_l;
+        protected $format;
+        protected $changeTIme;
+        
+        
+        // This will recive the pramaters from the class and make the usable to the function inside the class
+        public function __construct($changeTime, $format){
+            if($changeTime == null){
+            }
+            $this->format = $format;
+            $this->changeTime = $changeTime;
+        }
+
+        
+        
+                public function strtime($input){
+        // This will make it display the current date if no info is given
+            if($input == null){
+            $input = 'now';
+            }
+        $this->input = $input;
+        $anyTime = strtotime($this->input);
+        $this-> anyTime = $anyTime;
+        return $this->anyTime;
+        }  
+      
         
         public function anyDate($display, $input){
             // This will make it display all the info is no format is specified
@@ -20,7 +46,7 @@
             $this->display = $display;
             $this->input = $input;
             //this give you the ablilty to say next week or last thursday ect
-            $time = strtotime($this->input);  
+            $time = self::strtime($this->changeTime);  
             $anyDay = date($this->display, $time);
             $this->anyDay = $anyDay;
             return  $this->anyDay;
@@ -32,36 +58,59 @@
         //Might want to use the function above for the stuff down beldow here
         
         //this returns the full moth name eg "August"
-        public function month_F(){ 
-            $time = time();
+        public function set_month_F(){ 
+            $time = self::strtime($this->changeTime);
             $monthString = date('F',$time);
             $this->monthString = $monthString;
             return $this->monthString;
         }
+         public function get_month_F(){
+            self::set_month_F();
+            return $this->monthString;
+        
+        }
+        
         // This returns month abreavation eg Oct
-        public function month_M(){
-            $time = time();
+        public function set_month_M(){
+            $time = self::strtime($this->changeTime);
             $monthAbb = date('M',$time);
             $this->monthAbb = $monthAbb;
             return $this->monthAbb;
         }
+        public function get_month_M(){
+            self::set_month_M();
+            return $this->monthAbb;
+        
+        }
          // This returns Date /w leading 0 eg 01 -31
-        public function date_d(){
-            $time = time();
+        public function set_date_d(){
+            $time = self::strtime($this->changeTime);
             $date_d = date('d',$time);
             $this->date_d = $date_d;
             return $this->date_d;
         }
-            // This returns Date /w leading 0 eg 01 -31
-        public function day_l(){
-            $time = time();
-            $day_l = date('l',$time);
-            $this->day_l = $day_l;
-            return $this->day_l;
+        public function get_date_d(){
+            self::set_date_d();
+            return $this->date_d;
         }
+        
+            // This returns Date /w leading 0 eg 01 -31
+        public function set_day_l(){
+             
+            $time = self::strtime($this->changeTime);
+            $day_l = date('l',$time);
+            $this->day_l = $day_l;  
+        }
+        public function get_day_l(){
+             self::set_day_l();
+             return $this->day_l;
+  
+        }
+            
      }
-     $test = new date1();
+     $test = new date1('thursday');
      $test2 = new date1();
-     echo ($test->day_l() . "<br>" );
+     echo ($test->get_day_l() . "<br>" );
+     echo ($test->get_date_d() . "<br>" );
      echo $test->anyDate();
       ?>
